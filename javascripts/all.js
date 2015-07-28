80faceb73693df81820a124a7dcb9a4065206815
Ting Lew <ting.lew@gmail.com>
2015-07-28 16:18:38 -0400

    build

--- javascripts/all.js
+++ javascripts/all.js
@@ -1,42 +1,62 @@
 
 $(document).ready(function(){
-  $("#nav-contact").click(function(e){
-    e.preventDefault();
-    $("#contact-info").fadeToggle("fast", function () {});
-  });
+  // $("#nav-contact").click(function(e){
+  //   e.preventDefault();
+  //   $("#contact-info").fadeToggle("fast", function () {});
+  // });
 
 
-  $(".jumper").on("click", function(e) {
-    e.preventDefault();
-    $("body, html").animate({
-      scrollTop: $( $(this).attr('href') ).offset().top
-    },600);
-  });
+  // $(".jumper").on("click", function(e) {
+  //   e.preventDefault();
+  //   $("body, html").animate({
+  //     scrollTop: $( $(this).attr('href') ).offset().top
+  //   },600);
+  // });
 
-
+  // ONLOAD ANIMATIONS
   setTimeout(function() {
-    $("#hello").addClass('underline');
+    $(".hello").addClass('underline');
     $(".intro").toggle('slow') ;
   }, 500);
 
-
-// -webkit-transform: scaleX(0);
-//         -ms-transform: scaleX(0);
-//         transform: scaleX(0);
-
+  // PROJECT THUMBNAIL ANIMATIONS
   $(".project-pic").mouseover(function(){
-    // $(this).css("box-shadow", "0 2px 5px 0 rgba(0, 0, 0, 0.26)");
     $(this).parent().siblings().animate({
       opacity: .5
     },200
     );
   });
   $(".project-pic").mouseout(function(){
-    // $(this).css("box-shadow", "0 2px 5px 0 rgba(0, 0, 0, 0.26)");
     $(this).parent().siblings().animate({
       opacity: 1
     },200
     );
   });
+
+  // checks if jquery element is in view.
+  function isScrolledIntoView(elem)
+{
+    var $window = $(window);
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+    return ((elemBottom >= docViewTop) && (elemTop <= docViewBottom) && (elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
+
+
+
+  //on scroll event handler
+$(window).scroll(function() {
+    if(isScrolledIntoView('.project-pic'))
+    {
+        $(".projects-header-text").addClass('highlight');
+    }
+    if(isScrolledIntoView('#about-block'))
+    {
+        $(".about-header-text").addClass('underline');
+    }
+
+});
 
 });
